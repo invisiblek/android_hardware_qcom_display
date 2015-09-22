6e42b52696eba34e26a17f03a0f21b21186c491a
Chih-Hung Hsieh <chh@google.com>
2015-09-22 11:07:52 -0700

    Disable clang compiler to unblock angler system build.
    See bug description for the many compilation errors.
    
    BUG: 24171136
    Change-Id: I965679a9bc2e7fa434dee204597a3198c3bcfb48

--- liboverlay/Android.mk
+++ liboverlay/Android.mk
@@ -2,6 +2,9 @@ LOCAL_PATH := $(call my-dir)
 include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
 
+# b/24171136 many files not compiling with clang/llvm yet
+LOCAL_CLANG := false
+
 LOCAL_MODULE                  := liboverlay
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS             := optional
